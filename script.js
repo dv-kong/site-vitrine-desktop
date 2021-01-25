e961f89ca544584e650ab11a44b56ca0423c8672
dv-kong <zektbach@outlook.com>
2021-01-25 22:32:42 +0100

    25 jan Vincent

--- script.js
+++ script.js
@@ -2,16 +2,54 @@
 
 const navBar = document.querySelector('#main');
 
-function fixedNav() {  
-  if (window.scrollY >= topOfNav) {
-    document.body.style.paddingTop = navBar.offsetHeight + 'px'
-    document.body.classList.add('fixed-nav')
-  }
-  else {
-    document.body.classList.remove('fixed-nav')
-    document.body.style.paddingTop = 0
-  }
+function fixedNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = navBar.offsetHeight + 'px'
+        document.body.classList.add('fixed-nav')
+    } else {
+        document.body.classList.remove('fixed-nav')
+        document.body.style.paddingTop = 0;
+    }
 }
 
 window.addEventListener('scroll', fixedNav);
-const topOfNav = navBar.offsetTop  ;
+const topOfNav = navBar.offsetTop;
+
+
+
+
+
+
+
+
+
+// SCRIPT FOR PRODUITS.HTML
+
+let previousProductButton = document.querySelector(".previousItemButton")
+let nextProductButton = document.querySelector(".nextItemButton")
+previousItemButton.addEventListener("click", previousOrNextItem)
+nextItemButton.addEventListener("click", previousOrNextItem)
+
+function previousOrNextItem(input) {
+    if (input == previousItemButton) {
+
+    } else { // input is nextItemButton
+
+    }
+}
+
+
+
+let productTemplate = {
+    title: "TITLE",
+    description: "DESCRIPTION",
+    price: 30,
+    img: "PATH TO IMG",
+    alt: "ALT DESC",
+
+}
+
+
+
+
+// END OF SCRIPT FOR PRODUITS.HTML
